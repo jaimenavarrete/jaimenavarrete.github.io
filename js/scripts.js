5b6bb2220b0d9cfc55bd7bf255f37cb1625b603b
Jaime Navarrete <jaimenava123@outlook.com>
2020-09-03 15:58:49 -0600

    Finished - Resume section in all devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const btnMenu = document.getElementById('btn_menu'),
       barNavigation = document.getElementById('bar_navigation'),
-      header = document.getElementById('main_header')
+      header = document.getElementById('main_header'),
+      contactButton = document.getElementById('contactButton'),
+      info = document.getElementById('info')
 
 btnMenu.addEventListener('click', () => {
     barNavigation.classList.toggle('bar_navigation_active')
@@ -12,5 +14,20 @@ addEventListener('scroll', () => {
     }
     else {
         header.style.background = 'transparent'
+    }
+})
+
+contactButton.addEventListener('click', () => {
+    let infoStyles = window.getComputedStyle(info)
+
+    if(infoStyles.getPropertyValue('opacity') == '0') {
+        // info.style.display = 'block'
+        info.style.opacity = '1'
+        info.style.transform = 'translate(0)'
+    }
+    else {
+        // info.style.display = 'none'
+        info.style.opacity = '0'
+        info.style.transform = 'translateY(-100%)'
     }
 })
